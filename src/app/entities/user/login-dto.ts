74fb05ad183a4697c31165a732f6744bfdb18ce2
HellaxA <abereznikov64@gmail.com>
2021-05-05 00:04:18 +0300

    added search

--- src/app/entities/user/login-dto.ts
+++
@@ -1,9 +0,0 @@
-export class LoginDto {
-  login: string;
-  password: string;
-
-  constructor(login: string, password: string) {
-    this.login = login;
-    this.password = password;
-  }
-}
--- src/app/entities/user/user.ts
+++
@@ -1,7 +0,0 @@
-export class User {
-  password: string;
-  login: string;
-  name: string;
-  role: string;
-  accessToken?: string;
-}
